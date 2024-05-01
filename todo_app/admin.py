e4853a23989b7a2882b5aeb67b9c0db23c6f43f6
Roma <roma.li0071@gmail.com>
2024-05-01 20:11:08 +0500

    added Task object to admin panel, changed date format

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo_app.models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'description', )
+    list_filter = ('status', )
+    search_fields = ('description', )
+    exclude = []
+
+
+admin.site.register(Task, TaskAdmin)
