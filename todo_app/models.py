a633221ad0de555e9a8d260f619dabc5c238c199
Roma <roma.li0071@gmail.com>
2024-05-01 19:04:44 +0500

    created Task model, prepared a basic template for a future main page

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+STATUS_CHOICES = (
+    ('new', 'Новое'),
+    ('wip', 'В процессе'),
+    ('done', 'Выполнено'),
+)
+
+
+class Task(models.Model):
+    description = models.TextField('Описание', max_length=3000, null=False, blank=False)
+    status = models.CharField('Описание', choices=STATUS_CHOICES, default=STATUS_CHOICES[0], max_length=15, null=False)
+    deadline = models.DateField('Дата выполнения', blank=True, default='')
+
+    def __str__(self):
+        return f'{self.description} - {self.status}'
