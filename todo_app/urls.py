47a525274963d9c76af91edb50e603cdbe3e2b63
Roma <roma.li0071@gmail.com>
2024-05-01 19:54:57 +0500

    added 'add_task' view and simple adding form on the home page

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from todo_app.views import home_page
-
+from todo_app.views import home_page, add_task
 
 urlpatterns = [
     path('', home_page, name='home'),
+    path('add_task', add_task, name='add_task'),
 
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from todo_app.models import Task
 
@@ -7,3 +7,8 @@ def home_page(request):
     tasks = Task.objects.all()
     return render(request, 'index.html', {'tasks': tasks})
 
+
+def add_task(request):
+    data = request.POST
+    Task.objects.create(description=data['description'], deadline=data['deadline'])
+    return redirect('home')
