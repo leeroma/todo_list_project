a3e63b5b8ca773b05b35c81f92ed137926849e6c
Roma <roma.li0071@gmail.com>
2024-05-08 12:36:19 +0500

    Fixed date input on adding and editing pages

--- todo_app/forms.py
+++ todo_app/forms.py
@@ -3,7 +3,14 @@ from django import forms
 from todo_app.models import Task
 
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
 class TaskForm(forms.ModelForm):
+
+    deadline = forms.DateField(widget=DateInput(format='%Y-%m-%d'), required=False, label='Дата выполнения')
+
     class Meta:
         model = Task
         fields = ['title', 'description', 'status', 'deadline', ]
